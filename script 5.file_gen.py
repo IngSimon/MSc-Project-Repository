b3d1e9ffd6bb8052b5717e695ea0c50c5961efb9
Simon N <snteleman@gmail.com>
2019-03-04 11:52:14 +0000

    Update script 5.file_gen.py

--- script 5.file_gen.py
+++ script 5.file_gen.py
@@ -1,14 +1,11 @@
 """
 
-This script creates a txt file with num2-num1 number of lines.
+This script creates a txt file with a specified percentage.
 
-The name of output file: provided by user
+The name of directory: provided by user
 
 The default name of the input file: all_params.txt
 
-all_params.txt file should be in the same directory as the script
-
-Script in Python 2
 
 """
 
@@ -19,38 +16,48 @@ import sys
 import random
 
 
+def trim_data(percent, name2, filename2, filename3, num2= 1024, filename1="./data/all_params.txt"):
 
-def trim_data(num1, filename2, num2= 1024, filename1="./data/all_params.txt"):
+       num1 = int(round((num2*percent/100), 0)) 
+
+       with open(filename1) as file:
+
+           lines = file.read().splitlines()
+
+       random_lines = random.sample(lines, num1) 
+
+       with open(filename2, "w") as output_file1:  
+
+            output_file1.writelines(line + '\n' for line in lines if line not in random_lines)
+
+            output_file1.close()  
+
+       with open(filename3, "w") as output_file2:  
+
+            output_file2.writelines(line + '\n' for line in lines if line in random_lines)
+
+            output_file2.close()
 
 
+       num_of_lines = num2 - num1
 
-    with open(filename1) as file:
+       remainder = 100 - percent
 
-       lines = file.read().splitlines()
+       return remainder
 
-    random_lines = random.sample(lines, num1) 
-
-    with open(filename2, "w") as output_file:  
-
-        output_file.writelines(line + '\n' for line in lines if line not in random_lines)
-
-        output_file.close()  
-
-    num_of_lines = num2 - num1
-
-    return num_of_lines
 
  
-num1 = input('Please enter the number of lines to remove: ')
+percent = input('Please enter the percentage of entries to remove: ')
 
-name2 = input('Please enter the name of the output file (in quotes): ')
+name2 = input('Please enter the name of the directory for the output files: ')
 
 os.makedirs(name2)
 
-filename2 = './' + name2 + '/' +name2 + '.txt'
+filename2 = './' + name2 + '/' + "training" + '.txt'
 
-print 'A file {} with {} rows has been successfully generated'.format(filename2, trim_data(num1, filename2, num2= 1024, filename1="./data/all_params.txt"))
+filename3 = './' + name2 + '/' + "removed" + '.txt'
 
+print 'A file {} with {} percent of the original data has been successfully generated'.format(filename2, trim_data(percent, name2, filename2, filename3, num2= 1024, filename1="./data/all_params.txt"))
 
 
 

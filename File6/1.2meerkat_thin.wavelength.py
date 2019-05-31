f939d00cf23c3b5823c905e67ff6df49cff0ee50
Simon N <snteleman@gmail.com>
2019-05-31 15:08:29 +0100

    Add files via upload
    File 1.1 and 1.2 generate faraday thin and faraday thick data, respectively.
    File 2 deletes a percentage of data specified by the user.
    File 3 imputes missing data and creates a new directory. This script replaces missing entries with predicted using GPs.
    File 4 performs rm synthesis on the new data set and compares the initial fd spectra with the new one.

---
+++ File6/1.2meerkat_thin.wavelength.py
@@ -0,0 +1,282 @@
+#Generates simulated data of a faraday thin source
+#A value of rm=50 has been used.
+#Script outputs six files.
+
+import os,sys
+
+import numpy as np
+
+import pylab as pl
+
+import rm_tools as R
+
+
+
+# -----------------------------------------------------------------
+
+
+
+def get_1d_data(input_dir):
+
+
+
+        files = os.listdir(input_dir)
+
+        #if (len(files)!=3):
+
+            #print "Wrong number of input files in directory"
+
+            #return
+
+
+
+        nu_file = input_dir+"/frequencies.txt"
+
+        lam_file = input_dir+"/wavelengths.txt"
+
+        q_file = input_dir+"/stokesq.txt"
+
+        u_file = input_dir+"/stokesu.txt"
+
+
+
+        nu = np.loadtxt(nu_file, comments="#")
+
+        lam2 = np.loadtxt(lam_file, comments ='#')
+
+        stokesQ = np.loadtxt(q_file, comments="#")
+
+        stokesU = np.loadtxt(u_file, comments="#")
+
+
+
+        los = stokesQ + 1j*stokesU
+
+
+
+        return nu,los
+
+
+
+# -----------------------------------------------------------------
+
+
+
+def calc_phi_res(nu):
+
+
+
+        C2 = 8.98755179e16
+
+        nus = np.sort(nu)
+
+        delta_l2 = C2 * (nus[0] ** (-2) - nus[-1] ** (-2))
+
+
+
+        res = 2. * np.sqrt(3) / delta_l2
+
+
+
+        return res
+
+
+
+# -----------------------------------------------------------------
+
+
+
+def calc_maxscale(nu,dnu):
+
+
+
+        C2 = 8.98755179e16
+
+        nus = np.sort(nu)
+
+        delta_l2 = C2 * (nus[0] ** (-2) - nus[-1] ** (-2))
+
+
+
+        l2min = 0.5 * C2 * ((nus[-1] + dnu) ** (-2)
+
+                        + (nus[-1] - dnu) ** (-2))
+
+
+
+        maxscale = np.pi / l2min
+
+
+
+        return maxscale
+
+
+
+# -----------------------------------------------------------------
+
+# -----------------------------------------------------------------
+
+
+
+const_c = 3e8
+
+
+
+p0 = 1.0
+
+alpha = 0.7
+
+nu0 = 1.4e9
+
+rm = 50.
+
+phi0 = 0.
+
+
+
+#nu = np.linspace(120e6,180e6,1024) # LOFAR frequency coverage
+
+nu = np.linspace(580e6,250e7,1024) # MeerKAT frequency coverage
+
+lam = const_c/nu
+
+lam2 = lam**2
+
+
+stokesQ = p0*(nu/nu0)**(-alpha)*np.cos(2.*(rm*lam**2 + phi0))
+
+stokesU = p0*(nu/nu0)**(-alpha)*np.sin(2.*(rm*lam**2 + phi0))
+
+
+
+pl.subplot(111)
+
+pl.plot(lam2,stokesQ)
+
+pl.plot(lam2,stokesU)
+
+pl.xlabel('$\lambda^2 [m^2]$')
+
+pl.ylabel('Q and U ')
+
+pl.title('Stokes parameters against $\lambda^2 [m^2]$')
+
+pl.legend(['stokesQ','stokesU'])
+
+
+pl.savefig('plot1.png')
+
+pl.show()
+
+
+
+np.savetxt("./data/frequencies.txt",nu)
+
+np.savetxt("./data/wavelengths.txt",lam2)
+
+np.savetxt("./data/stokesq.txt",stokesQ)
+
+np.savetxt("./data/stokesu.txt",stokesU)
+
+
+#combine the three parameters into one file
+
+with open("./data/all_params.txt", 'w') as file5:
+
+  with open("./data/frequencies.txt", 'r') as file1:
+    
+    with open("./data/wavelengths.txt", 'r') as file2:
+
+       with open("./data/stokesq.txt", 'r') as file3:
+
+          with open("./data/stokesu.txt", 'r') as file4:
+
+           
+
+            for line1, line2, line3, line4  in zip(file1, file2, file3, file4):
+
+                print >>file5, line1.strip(), line2.strip(),line3.strip(), line4.strip()
+
+
+
+
+# specify the range of fd space:
+
+phi = np.linspace(-1000,1000,4000)
+
+
+
+# get the input data:
+
+inputdir = "./data"
+
+nu,los = get_1d_data(inputdir)
+
+dnu = nu[1]-nu[0]
+
+
+
+# initialise the pyrmsynth class:
+
+rms = R.RMSynth(nu,dnu,phi)
+
+
+
+# calculate the resolution in fd space and the maximum recoverable scale:
+
+res = calc_phi_res(nu)
+
+maxscale = calc_maxscale(nu,dnu)
+
+print "\n"
+
+print "Max f.d. resolution: " +str(round(res)) + " rad/m^2"
+
+print "Max f.d. scale " +str(round(maxscale)) + " rad/m^2"
+
+print "\n"
+
+
+
+# plot the RMTF:
+
+pl.subplot(111)
+
+pl.plot(rms.rmsf_phi,np.abs(rms.rmsf))
+
+pl.axis([-1000,1000,-0.1,1.1])
+
+pl.xlabel('RMSF ($\phi$)')
+
+pl.ylabel('RMSF')
+
+pl.savefig('plot2.png')
+
+pl.show()
+
+
+
+# run the RM synthesis transform on the data:
+
+fd_spec = rms.compute_dirty_image(los)
+
+#save the initial farady depth spectra
+
+np.save("./data/fd_spec_initial.npy",fd_spec)
+
+
+# plot the output:
+
+pl.subplot(111)
+ 
+pl.plot(phi,np.abs(fd_spec))
+
+pl.xlabel('$\phi$ [rad $m^{-2}$]')   # string must be enclosed with quotes '  '
+
+pl.ylabel('F ($\phi$)')
+
+pl.axis([-1000,1000,-0.1,1.5])
+
+pl.savefig('plot3.png')
+
+pl.show()
